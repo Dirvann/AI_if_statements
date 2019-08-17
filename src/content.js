6b45d7519727e8c9dcc9573c541289a9b99a1b24
Cat Stevens <catb0t@protonmail.ch>
2019-08-16 21:15:28 -0400

    fix PAIN -> Psome IF statementsN bug, optimise searching code

--- src/content.js
+++ src/content.js
@@ -1,21 +1,31 @@
-function replaceTextOnPage(from, to){
-  getAllTextNodes().forEach(function(node){
-    node.nodeValue = node.nodeValue.replace(new RegExp(quote(from), 'g'), to);
-  });
+function rsearchChildNodesByType(node, type, include) {
+  var result = [];
 
-  function getAllTextNodes(){
-    var result = [];
+  if (node.childNodes.length) {
+    for(var i = 0; i < node.childNodes.length; i++) {
+      result = result.concat(rsearchChildNodesByType(node.childNodes[i], type, include));
+    }
 
-    (function scanSubTree(node){
-      if(node.childNodes.length) 
-        for(var i = 0; i < node.childNodes.length; i++) 
-          scanSubTree(node.childNodes[i]);
-      else if(node.nodeType == Node.TEXT_NODE) 
-        result.push(node);
-    })(document);
-
-    return result;
+  } else {
+    if ((node.nodeType == type) && node.nodeValue && node.nodeValue.includes(include)) {
+      //console.log('HIT ' + node.nodeValue);
+      result.push(node);
+    } else {
+      // console.log('IGN got ' + node.nodeType + ' want ' + type + ' interesting? ' + (node.nodeValue && node.nodeValue.includes(include)) + ' reason: ' + include + ' val: ' + node.nodeValue);
+    }
   }
+  // result.length && console.log(result);
+  return result;
+}
+
+function replaceTextOnPage (from, to) {
+  rsearchChildNodesByType(document, Node.TEXT_NODE, from).forEach(function (node) {
+    console.log('GOT ' + node.nodeValue);
+    node.nodeValue = node.nodeValue.replace(
+      new RegExp('(\\b)' + quote(from) + '(\\b)', 'g'),
+      '$1' + to + '$2'
+    );
+  });
 
   function quote(str){
     return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
@@ -26,5 +36,7 @@ function replaceTextOnPage(from, to){
 replaceTextOnPage('AI', 'some IF statements');
 replaceTextOnPage('A.I.', 'some IF statements');
 replaceTextOnPage('Artificial Intelligence', 'some IF statements');
-replaceTextOnPage('artificial intelligence', 'some IF statements');
 replaceTextOnPage('Artificial intelligence', 'some IF statements');
+replaceTextOnPage('artificial intelligence', 'some IF statements');
+replaceTextOnPage('artificial Intelligence', 'some IF statements');
+replaceTextOnPage('ARTIFICIAL INTELLIGENCE', 'some IF statements');
